e4a99c4b1725272247452b1ede497c79bb14c1e8
Darshan <sndarshan1126@gmail.com>
2023-12-12 19:49:54 +0530

    added components

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,32 +1,33 @@
-'use client';
+"use client";
 import EditableImage from "@/components/layout/EditableImage";
 import InfoBox from "@/components/layout/InfoBox";
 import SuccessBox from "@/components/layout/SuccessBox";
 import UserForm from "@/components/layout/UserForm";
 import UserTabs from "@/components/layout/UserTabs";
-import {useSession} from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import {redirect} from "next/navigation";
-import {useEffect, useState} from "react";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
+//Profile page
 export default function ProfilePage() {
   const session = useSession();
 
   const [user, setUser] = useState(null);
   const [isAdmin, setIsAdmin] = useState(false);
   const [profileFetched, setProfileFetched] = useState(false);
-  const {status} = session;
+  const { status } = session;
 
   useEffect(() => {
-    if (status === 'authenticated') {
-      fetch('/api/profile').then(response => {
-        response.json().then(data => {
+    if (status === "authenticated") {
+      fetch("/api/profile").then((response) => {
+        response.json().then((data) => {
           setUser(data);
           setIsAdmin(data.admin);
           setProfileFetched(true);
-        })
+        });
       });
     }
   }, [session, status]);
@@ -35,31 +36,28 @@ export default function ProfilePage() {
     ev.preventDefault();
 
     const savingPromise = new Promise(async (resolve, reject) => {
-      const response = await fetch('/api/profile', {
-        method: 'PUT',
-        headers: {'Content-Type': 'application/json'},
+      const response = await fetch("/api/profile", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
       });
-      if (response.ok)
-        resolve()
-      else
-        reject();
+      if (response.ok) resolve();
+      else reject();
     });
 
     await toast.promise(savingPromise, {
-      loading: 'Saving...',
-      success: 'Profile saved!',
-      error: 'Error',
+      loading: "Saving...",
+      success: "Profile saved!",
+      error: "Error",
     });
-
   }
 
-  if (status === 'loading' || !profileFetched) {
-    return 'Loading...';
+  if (status === "loading" || !profileFetched) {
+    return "Loading...";
   }
 
-  if (status === 'unauthenticated') {
-    return redirect('/login');
+  if (status === "unauthenticated") {
+    return redirect("/login");
   }
 
   return (
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -4,6 +4,8 @@ import Image from "next/image";
 import Link from "next/link";
 import {useState} from "react";
 
+
+//Register 
 export default function RegisterPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
